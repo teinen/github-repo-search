01386f512c80c8078e5bdb2a4ea564d769d84e90
諦念 <teinen.github@gmail.com>
2018-06-06 21:54:11 +0900

    Add prototype

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
-  
+  <div>
+    <h1>Github Repository Search</h1>
+    <div class="list">
+      <result
+        v-for="result in results"
+        :key="result.id"
+        :result="result"
+      ></result>
+    </div>
+  </div>
 </template>
 
 <script>
+import Result from './components/Result'
+
 export default {
-  
+  data () {
+    return {
+      results: [
+        {
+          id: 1,
+          url: 'https://github.com/vuejs/vue',
+          repo_name: 'vuejs/vue',
+          stars: 96725,
+          description: 'testtest'
+        }
+      ]
+    }
+  },
+  components: {
+    'result': Result
+  }
 }
 </script>
 
 <style lang="scss">
-
+  h1 {
+    text-align: center;
+  }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <div>
+      <a :href=result.url target="_blank">
+        {{ result.repo_name }}
+      </a>
+      🌟<strong>{{ result.stars }}</strong>
+    </div>
+    <p>{{ result.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'result'
+  ]
+}
+</script>
+
+<style lang="scss">
+  .content {
+    padding: 10;
+    margin: 10;
+    background: white;
+    box-shadow: 0 1px 5px rgba(0,0,0,0.5);
+  }
+</style>
